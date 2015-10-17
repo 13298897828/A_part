75120311b22f027bee38c18b643b6808dc69ada4
Tianqi Zhang <1120404010@qq.com>
2015-10-17 15:34:07 +0800

    Initial Commit

---
+++ Project_A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project_A
+//
+//  Created by 张天琦 on 15/10/17.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project_A/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project_A
+//
+//  Created by 张天琦 on 15/10/17.
+//  Copyright © 2015年 张天琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
